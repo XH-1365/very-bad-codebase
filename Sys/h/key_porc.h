23d17a222939f4b1386f2928173af4161a7f01c0
HZD <2684791179@qq.com>
2023-03-11 16:33:19 +0800

    按钮的基本复习

---
+++ Sys/h/key_porc.h
@@ -0,0 +1,4 @@
+#include "stc15f2k60s2.h"
+
+
+void Key_str(unsigned char *str);
---
+++ Sys/key_porc.c
@@ -0,0 +1,39 @@
+#include "key_porc.h"
+
+unsigned char pattern[8] = {0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,};
+unsigned char PATTERNS[10] = {0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,};//此处需要写0-9数值,但是本人太懒不想写
+
+
+//void Key_porc()
+//{
+//	
+//}
+
+
+void Key_str(unsigned char *str)
+{
+	unsigned char i;
+	for(i = 0; i > 8;i++)
+	{
+		unsigned char dig;
+		if (*str)
+		{
+			if (*str >= '0' && *str <= '9')
+			{
+				dig = PATTERNS[*str - '0'];	
+			}
+			str++;
+				/*
+				else
+				{
+					//可以输入你需要的字母，这里就不演示了
+				}
+				*/
+			if(*str == '.')
+			{
+				dig = ~0x80;
+			}
+			pattern[i] = dig;
+		}
+	}
+}
